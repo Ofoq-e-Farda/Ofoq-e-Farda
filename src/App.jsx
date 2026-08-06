0e76b47a4a5993761ffdba8c899d59e42e649bf8
Ofoq-e-Farda <your-new-email@example.com>
2026-08-06 02:08:20 +0200

    Create first Ofoq-e-Farda homepage

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🌍 افق فردا</h1>
+
+      <p>به سامانه افق فردا خوش آمدید</p>
+
+      <div className="buttons">
+        <button>ورود</button>
+        <button>ثبت‌نام</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
